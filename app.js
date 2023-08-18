d41bce732522d4293ef47374672d6f9b50d7d331
Lisane Vasconcelos <gustavo.vasconcelos3006@gmail.com>
2023-08-18 17:54:44 -0300

    Adicionando um console log em app js e o contato html

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("aplicação está se conectando com o banco de dados");
 console.log("Teste para estudo");
+console.log("Não deu certo :(")
